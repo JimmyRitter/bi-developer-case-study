9d9ac93802b409afb7cc82f137e811afb9b4616c
Jimmy Ritter <jimmy.ritter@redacreltd.com>
2023-05-18 16:04:55 +0200

    small fixes on part A exercise

--- part-A/resolution/query.sql
+++ part-A/resolution/query.sql
@@ -1,21 +1,26 @@
+/*
+    Description:
+    Query for generating a report which summarises the performance of each action group.
+*/
+
 SELECT
     a.action_group
-    ,COUNT(a.user_id) as PlayersTargeted
-    ,COUNT(distinct d.user_id)  as Conversions
-    ,COUNT(distinct d.user_id) / COUNT(distinct a.user_id) as ConversionRate
-    ,AVG(distinct d.amount) as AverageFirstDepositAmount
-    ,SUM(d.depositCount) / COUNT(distinct d.user_id)  as DepositCountConversion
-    ,SUM(d.depositAmount) / COUNT(distinct d.user_id)  as DepositAmountConversion
-    ,MAX(distinct d.SingleBiggestDeposit) as SingleBiggestDeposit
+    ,COUNT(a.user_id) as players_targeted
+    ,COUNT(distinct d.user_id)  as conversions
+    ,COUNT(distinct d.user_id) / COUNT(distinct a.user_id) as conversion_rate
+    ,AVG(distinct d.amount) as average_first_deposit_amount
+    ,SUM(d.deposit_count) / COUNT(distinct d.user_id) as deposit_count_conversion
+    ,SUM(d.deposit_amount) / COUNT(distinct d.user_id)  as deposit_amount_conversion
+    ,MAX(distinct d.single_biggest_deposit) as single_biggest_deposit
 FROM
     activities_sent a
 JOIN user_details ud on a.user_id = ud.user_id
 LEFT JOIN (
     SELECT
         user_details.user_Id
-        ,COUNT(amount) as DepositCount
-        ,SUM(amount) as DepositAmount
-        ,MAX(amount) as SingleBiggestDeposit
+        ,COUNT(amount) as deposit_count
+        ,SUM(amount) as deposit_amount
+        ,MAX(amount) as single_biggest_deposit
         ,amount
     FROM 
         user_details
